5fc5047d785bdbc8205a660afcbcfb07f07a5a64
XiaoXin <xiaoxin@MacBookPro.bbrouter>
2025-05-14 23:46:45 +0800

    update comment

--- plugins/handler.go
+++ plugins/handler.go
@@ -9,6 +9,21 @@ import (
 	"github.com/libra412/fn/plugins/transport"
 )
 
+// Handler is the main entry point for handling requests.
+// It unmarshals the request body, retrieves the appropriate endpoint from the router,
+// and calls the endpoint with the provided context, logger, header, and request data.
+// It returns the response as a byte slice or an error if something goes wrong.
+// The request body should be a JSON object with "method" and "data" fields.
+// The "method" field specifies the endpoint to call, and the "data" field contains the request data.
+// The response is expected to be a JSON object as well.
+// for example:
+//
+//	{
+//	  "method": "user.get",
+//	  "data": {
+//	    "id": 1
+//	  }
+//	}
 func Handler(ctx context.Context, logger *log.Logger, header map[string][]string, body []byte) ([]byte, error) {
 	var request struct {
 		Method string `json:"method"`
